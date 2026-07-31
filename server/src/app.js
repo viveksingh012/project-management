755116da9d2aeb9ad6b74b182bc476ec059f2810
Vivek Singh <your-email@example.com>
2026-07-31 12:01:59 +0530

    deploye: cors change frontend connect to backend

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        origin: process.env.CORS_ORIGIN || "https://project-management-bice-two.vercel.app",
         credentials: true,
     })
 );
